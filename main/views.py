bd6f2a7f34ebdf4f01d7991912ca050cad4c7aaa
ujstor <astipan@gmail.com>
2023-04-04 20:54:30 +0200

    python syntax in html

--- main/views.py
+++ main/views.py
@@ -6,7 +6,10 @@ from .models import ToDoList, Item
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    return render(response, "main/base.html", {"name":ls.name})
+    return render(response, "main/list.html", {"ls":ls})
 
 def home(response):
     return render(response, "main/home.html", {})
+
+#def list(response):
+    #return render(response, "main/list.html", {})
