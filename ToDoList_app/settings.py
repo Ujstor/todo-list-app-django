03512f73e8cfa715953f8f997524d0f9fe82b2a8
ujstor <astipan@gmail.com>
2023-09-02 13:57:16 +0200

    ALLOWED_HOSTS

--- ToDoList_app/settings.py
+++ ToDoList_app/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://django-todo.astipan.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['django-todo.astipan.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
